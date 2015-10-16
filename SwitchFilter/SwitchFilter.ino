69f925ad255463118ff525736c97ca788e706370
OwenMcGirr <o.a.mcgirr@gmail.com>
2015-10-16 13:44:42 +0100

    Add Arduino sketch

---
+++ SwitchFilter/SwitchFilter.ino
@@ -0,0 +1,56 @@
+#define OUTPUT_PULSE_DELAY 250
+#define DEBOUNCE_DELAY 10
+
+// switch and output pins
+int inputSwitch = 8;
+int outputPort = 12;
+
+// pin state variables
+boolean currentInputSwitchState = LOW;
+boolean previousInputSwitchState = LOW;
+
+void setup() {
+  // initialise IO
+  pinMode(inputSwitch, INPUT);
+  pinMode(outputPort, OUTPUT);
+}
+
+void loop() {
+  // debounce switch
+  currentInputSwitchState = debounce(inputSwitch, previousInputSwitchState);
+
+  // if input switch was just released, activate output
+  if (currentInputSwitchState == LOW && previousInputSwitchState == HIGH) {
+    doOutputPortDownUp();
+  }
+
+  // set previous input switch state
+  previousInputSwitchState = currentInputSwitchState;
+}
+
+
+/*
+ * Output port functions
+ */
+
+void doOutputPortDownUp() {
+  digitalWrite(outputPort, HIGH);
+  delay(OUTPUT_PULSE_DELAY);
+  digitalWrite(outputPort, LOW);
+}
+
+
+/*
+ * Debounce function
+ */
+
+boolean debounce(int pin, boolean previous) {
+  boolean current = digitalRead(pin);
+
+  if (current != previous) {
+    delay(DEBOUNCE_DELAY);
+    current = digitalRead(pin);
+  }
+
+  return current;
+}
